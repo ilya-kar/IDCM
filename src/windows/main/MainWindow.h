f9e64a8a3be895ee8fd10d2be4cdf775ccee4749
dispronesson <dispronesson@mail.ru>
2025-09-13 20:38:12 +0300

    fix build bug

--- src/windows/main/MainWindow.h
+++ src/windows/main/MainWindow.h
@@ -7,7 +7,7 @@ class MainWindow : public QMainWindow {
     Q_OBJECT
 public:
     explicit MainWindow(QWidget *parent = nullptr);
-    ~MainWindow() { delete ui; }
+    ~MainWindow() override { delete ui; }
 
 private:
     Ui::MainWindow *ui;
