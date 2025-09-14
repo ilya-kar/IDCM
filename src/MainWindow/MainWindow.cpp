39e34ec25c1c27fa025efbbcc8cc8a770be13bfe
dispronesson <dispronesson@mail.ru>
2025-09-15 02:39:54 +0300

    added .qrc

--- src/MainWindow/MainWindow.cpp
+++ src/MainWindow/MainWindow.cpp
@@ -2,8 +2,5 @@
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
-    connect(ui->clickButton, &QPushButton::clicked, ui->clickButton, [=]() {
-        ui->clickButton->setText("Clicked!");
-    });
 }
 
